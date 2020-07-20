68fb8448dc2c90d06901207171e5f515c42d30a1
Rikko <celvineadiputra@mhs.mdp.ac.id>
2020-07-20 14:59:17 -0700

    init

---
+++ JS/Base.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDS9-zV5ngWKH4ZeOFEefLkAfA66ZPXZtQ",
+  authDomain: "healthcareme-6f756.firebaseapp.com",
+  databaseURL: "https://healthcareme-6f756.firebaseio.com",
+  projectId: "healthcareme-6f756",
+  storageBucket: "healthcareme-6f756.appspot.com",
+  messagingSenderId: "392586847432",
+  appId: "1:392586847432:web:25d01177fb88e5313ef1fd",
+  measurementId: "G-WJ278W80ZV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
